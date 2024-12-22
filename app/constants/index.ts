cba019d95686bffc0950ee19fbd3a82a46c92562
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-22 12:13:11 +0530

    Add constants for social links and color palette

---
+++ app/constants/index.ts
@@ -0,0 +1,10 @@
+export const SOCIAL_LINKS = {
+    INSTAGRAM: "https://www.instagram.com/doubletap.work/",
+    LINKEDIN: "https://www.linkedin.com/company/doubletap-work/",
+    TEXT: "sms:",
+};
+
+export const COLORS = {
+    PRIMARY: "#DE5C35",
+    BLACK: "#1A1A1A",
+};
