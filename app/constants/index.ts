d9c1cb1145bb6dc80545d6c2576bb588958fd664
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-22 12:27:59 +0530

    Update social link for WhatsApp

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,7 +1,7 @@
 export const SOCIAL_LINKS = {
     INSTAGRAM: "https://www.instagram.com/doubletap.work/",
     LINKEDIN: "https://www.linkedin.com/company/doubletap-work/",
-    TEXT: "sms:",
+    TEXT: "https://wa.me/919124999144",
 };
 
 export const COLORS = {
