428914a261af2a803c02b2e5944c4b6aa5d3d3a3
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-05-02 03:49:17 +0530

    Add Hero component

---
+++ app/sections/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <section className="min-h-screen bg-[#DADADA] flex flex-col">
+      <div className="flex-1 flex items-center justify-center p-8 max-w-7xl mx-auto">
+        <h1 className="text-2xl md:text-3xl lg:text-6xl font-normal text-center mx-auto font-power-grotesk">
+          at double tap we make our work speak
+        </h1>
+      </div>
+    </section>
+  );
+}
