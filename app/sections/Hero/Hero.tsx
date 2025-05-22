eb7aa9acc4c2adaea6cebbdaad1519ffee5dc33a
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-05-22 18:07:10 +0530

    Implement smooth scrolling functionality in Hero section with clickable text elements for navigation

--- app/sections/Hero/Hero.tsx
+++ app/sections/Hero/Hero.tsx
@@ -1,9 +1,59 @@
+"use client";
+
 export default function Hero() {
+  const scrollToSection = (sectionId: string) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <section className="min-h-screen bg-[#DADADA] flex flex-col">
-      <div className="flex-1 flex items-center justify-center p-8 max-w-7xl mx-auto">
+    <section className="min-h-screen h-dvh bg-[#DADADA] flex flex-col ">
+      <div className="flex-1 flex items-center justify-center max-w-6xl mx-auto">
         <h1 className="text-2xl md:text-3xl lg:text-6xl font-normal text-center mx-auto font-power-grotesk">
-          at double tap we make our work speak
+          <span
+            className="cursor-pointer hover:font-bold hover:text-[#F15A24] transition-all duration-300"
+            onClick={() => scrollToSection("address")}
+          >
+            at
+          </span>{" "}
+          <span
+            className="cursor-pointer hover:font-bold hover:text-[#F15A24] transition-all duration-300"
+            onClick={() => scrollToSection("about")}
+          >
+            double tap
+          </span>{" "}
+          <span
+            className="cursor-pointer hover:font-bold hover:text-[#F15A24] transition-all duration-300"
+            onClick={() => scrollToSection("team")}
+          >
+            we
+          </span>{" "}
+          <span
+            className="cursor-pointer hover:font-bold hover:text-[#F15A24] transition-all duration-300"
+            onClick={() => scrollToSection("what-we-do")}
+          >
+            let
+          </span>{" "}
+          <span
+            className="cursor-pointer hover:font-bold hover:text-[#F15A24] transition-all duration-300"
+            onClick={() => scrollToSection("careers")}
+          >
+            our
+          </span>{" "}
+          <span
+            className="cursor-pointer hover:font-bold hover:text-[#F15A24] transition-all duration-300"
+            onClick={() => scrollToSection("works")}
+          >
+            work
+          </span>{" "}
+          <span
+            className="cursor-pointer hover:font-bold hover:text-[#F15A24] transition-all duration-300"
+            onClick={() => scrollToSection("contact")}
+          >
+            speak
+          </span>
         </h1>
       </div>
     </section>
