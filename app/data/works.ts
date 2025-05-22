9466b30403a9b1db8404dbcf423be92d2d024698
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-05-22 18:05:06 +0530

    Add images to works data and enhance Works section with hover effects and animations

--- app/data/works.ts
+++ app/data/works.ts
@@ -4,23 +4,27 @@ export const works = [
         description: "brand identity design",
         client: "",
         services: [],
+        image: "/images/team/jyoti.png",
     },
     {
         title: "khimji aabartana ",
         description: "brand identity design",
         client: "khimji aabartana",
         services: ["content", "concept", "design", "print"],
+        image: "/images/team/sailesh.png",
     },
     {
         title: "kiit school of public policy",
         description: "brand identity design",
         client: "",
         services: [],
+        image: "/images/team/jyoti.png",
     },
     {
         title: "kiit school of public policy",
         description: "brand identity design",
         client: "",
         services: [],
+        image: "/images/team/sailesh.png",
     }
 ]; 
