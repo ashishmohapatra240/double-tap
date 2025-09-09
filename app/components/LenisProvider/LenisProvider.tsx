5150c1a92c4cb3878128813b02a1dd5851dc74f3
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-09-09 11:32:28 +0530

    Comment out LenisProvider

--- app/components/LenisProvider/LenisProvider.tsx
+++ app/components/LenisProvider/LenisProvider.tsx
@@ -1,33 +1,33 @@
-"use client";
-import { ReactNode, useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+// "use client";
+// import { ReactNode, useEffect } from "react";
+// import Lenis from "@studio-freight/lenis";
 
-interface LenisProviderProps {
-  children: ReactNode;
-}
+// interface LenisProviderProps {
+//   children: ReactNode;
+// }
 
-export default function LenisProvider({ children }: LenisProviderProps) {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.5,
-      easing: (t) => t * (2 - t),
-      orientation: "vertical",
-      gestureOrientation: "vertical",
-      touchMultiplier: 1.5,
-      lerp: 0.1,
-    });
+// export default function LenisProvider({ children }: LenisProviderProps) {
+//   useEffect(() => {
+//     const lenis = new Lenis({
+//       duration: 1.5,
+//       easing: (t) => t * (2 - t),
+//       orientation: "vertical",
+//       gestureOrientation: "vertical",
+//       touchMultiplier: 1.5,
+//       lerp: 0.1,
+//     });
 
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
+//     function raf(time: number) {
+//       lenis.raf(time);
+//       requestAnimationFrame(raf);
+//     }
 
-    requestAnimationFrame(raf);
+//     requestAnimationFrame(raf);
 
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
+//     return () => {
+//       lenis.destroy();
+//     };
+//   }, []);
 
-  return <>{children}</>;
-}
+//   return <>{children}</>;
+// }
